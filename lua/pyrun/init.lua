3db37288d26edae6102f069917ef1061878a4cd8
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-07-24 00:06:36 +0200

    (feat) Starts to put together full module path for test file

--- lua/pyrun/init.lua
+++ lua/pyrun/init.lua
@@ -1,11 +1,22 @@
 local M = {}
 
+local fn = vim.api.nvim_buf_get_name(0)
+local cwd = vim.uv.cwd()
+local is_django = vim.uv.fs_stat(cwd .. "/manage.py") ~= nil
+
 M.run = function()
-    local ft = vim.bo.filetype
-    if ft == "python" then
-        local fn = vim.api.nvim_buf_get_name(0)
-        print("filename: " .. fn)
+    local parts = vim.split(fn, cwd)
+    print(parts[2])
+    local module_path = ""
+    for _, i in ipairs(vim.split(parts[2], "/")) do
+        if module_path == "" then
+            module_path = i
+        else
+            module_path = module_path .. "." .. i
+        end
     end
+    print(module_path)
+
 end
 
 return M
