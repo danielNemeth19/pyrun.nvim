b9cefea23e3cb4bdea05ca7a37c757c79f37fa52
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-07-24 23:54:05 +0200

    (feat) Implements command string to run all tests for file

--- lua/pyrun/init.lua
+++ lua/pyrun/init.lua
@@ -1,22 +1,48 @@
 local M = {}
 
-local fn = vim.api.nvim_buf_get_name(0)
-local cwd = vim.uv.cwd()
-local is_django = vim.uv.fs_stat(cwd .. "/manage.py") ~= nil
+-- local is_django = vim.uv.fs_stat(cwd .. "/manage.py") ~= nil
 
-M.run = function()
-    local parts = vim.split(fn, cwd)
-    print(parts[2])
-    local module_path = ""
-    for _, i in ipairs(vim.split(parts[2], "/")) do
-        if module_path == "" then
-            module_path = i
-        else
-            module_path = module_path .. "." .. i
-        end
+function M.run()
+  local fp = vim.api.nvim_buf_get_name(0)
+  local manage_fp = M.find_manage_file(fp)
+  if manage_fp ~= nil then
+    -- vim.notify(manage_fp, vim.log.levels.INFO)
+    local module_path = M.set_module_path(fp, manage_fp)
+    vim.notify("python manage.py " .. module_path, vim.log.levels.INFO)
+  end
+end
+
+---@param filepath string
+---@return string|nil
+function M.find_manage_file(filepath)
+  local result = vim.fs.find("manage.py", {
+    limit = 1,
+    upward = true,
+    type = "file",
+    path = filepath
+  })
+  local manage_file = result[1]
+  -- vim.notify("manage_fp is " .. manage_file, vim.log.levels.INFO)
+  return manage_file
+end
+
+---@return string module_path
+---@param fp string
+---@param manage_fp string
+function M.set_module_path(fp, manage_fp)
+  local module_path = string.gsub(fp, ".py", "")
+  -- vim.notify("module_path is" .. module_path, vim.log.levels.INFO)
+  local project_root = vim.fs.dirname(manage_fp)
+  local parts = vim.split(module_path, project_root)
+  local module = ""
+  for _, i in ipairs(vim.split(parts[2], "/")) do
+    if module == "" then
+      module = i
+    else
+      module = module .. "." .. i
     end
-    print(module_path)
-
+  end
+  return module
 end
 
 return M
--- plugin/loading.lua
+++ plugin/loading.lua
@@ -1,7 +1,5 @@
-vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
-    pattern = { "*.py" },
-    callback = function()
-        local pr = require("pyrun")
-        vim.keymap.set("n", "<leader>t", pr.run)
-    end
-})
+vim.api.nvim_create_autocmd({ 'BufReadPost' },
+  { pattern = { "*.py" }, callback = function()
+    local pr = require("pyrun")
+    vim.keymap.set("n", "<leader>t", pr.run)
+  end })
